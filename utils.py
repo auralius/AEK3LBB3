d223673f2eb4d6d9486c550191249172d52aa6d8
Auralius Manurung <auralius@users.noreply.github.com>
2025-01-27 15:46:50 +0700

    nearest neighbors

---
+++ utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numba import njit, prange
+
+
+@njit(parallel=True)
+def compute_distances(X, Y, L=2):
+    nX = X.shape[0]
+    nY = Y.shape[0]
+    
+    dists = np.zeros((nX, nY))
+    
+    for i in prange(nX):
+        for j in range(nY):
+            if L == 1:   # L1-norm
+                dists[i, j] = np.sum(np.abs(X[i] - Y[j]))
+            elif L == 2: # L2-norm
+                dists[i, j] = np.sqrt(np.sum((X[i] - Y[j])**2))
+    
+    return dists
